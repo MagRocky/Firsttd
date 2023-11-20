fb2fbd92dca28173d5bde114bd6f40297f565ddc
MagRocky <getitrocky@gmail.com>
2023-11-19 15:28:45 -1200

    python calc

---
+++ txt.py
@@ -0,0 +1,92 @@
+import time
+
+def add():
+    print("""ADDITION
+~~~~~~~~""")
+    a=int(input("Enter A:"))
+    b=int(input("Enter B:"))
+    print(a+b)
+add()
+print("_______________________________________________________________________________")
+
+def sub():
+    time.sleep(0.5)
+    print("""SUBRACTION
+~~~~~~~~""")
+    a=int(input("Enter A:"))
+    b=int(input("Enter B:"))
+    print(a-b)
+sub()
+print("_______________________________________________________________________________")
+
+
+
+def mul():
+    time.sleep(0.5)
+    print("""MULTIPLICATION
+~~~~~~~~""")
+    a=int(input("Enter A:"))
+    b=int(input("Enter B:"))
+    print(a*b)
+mul()
+print("_______________________________________________________________________________")
+
+
+def div():
+    time.sleep(0.5)
+    print("""DIVISION
+~~~~~~~~""")
+    a=int(input("Enter A:"))
+    b=int(input("Enter B:"))
+    print(a/b)
+div()
+print("_______________________________________________________________________________")
+
+
+
+import time
+
+def add():
+    print("""ADDITION
+~~~~~~~~""")
+    a=int(input("Enter A:"))
+    b=int(input("Enter B:"))
+    print(a+b)
+add()
+print("_______________________________________________________________________________")
+
+def sub():
+    time.sleep(0.5)
+    print("""SUBRACTION
+~~~~~~~~""")
+    a=int(input("Enter A:"))
+    b=int(input("Enter B:"))
+    print(a-b)
+sub()
+print("_______________________________________________________________________________")
+
+
+
+def mul():
+    time.sleep(0.5)
+    print("""MULTIPLICATION
+~~~~~~~~""")
+    a=int(input("Enter A:"))
+    b=int(input("Enter B:"))
+    print(a*b)
+mul()
+print("_______________________________________________________________________________")
+
+
+def div():
+    time.sleep(0.5)
+    print("""DIVISION
+~~~~~~~~""")
+    a=int(input("Enter A:"))
+    b=int(input("Enter B:"))
+    print(a/b)
+div()
+print("_______________________________________________________________________________")
+
+
+
